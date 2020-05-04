3b6c4c3be7c1e4381e334bce461529f6621605db
Virtual-Memes <549604608@qq.com>
2020-05-04 20:28:27 +0800

    updated

--- app/src/main/java/com/example/linear_equation/MainActivity.java
+++ app/src/main/java/com/example/linear_equation/MainActivity.java
@@ -18,6 +18,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int Anim_Flag=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,23 +32,30 @@ public class MainActivity extends AppCompatActivity {
         FG_3.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                FG_3.setBackgroundResource(R.drawable.bt_second);
-                FG_4.setBackgroundResource(R.drawable.bt_first);
-                replaceFrag(new Frag_3());
+                if(Anim_Flag!=1) {
+                    Anim_Flag = 1;
+                    FG_3.setBackgroundResource(R.drawable.bt_second);
+                    FG_4.setBackgroundResource(R.drawable.bt_first);
+                    replaceFrag(new Frag_3());
+                }
             }
         });
         FG_4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FG_4.setBackgroundResource(R.drawable.bt_second);
-                FG_3.setBackgroundResource(R.drawable.bt_first);
-                replaceFrag(new Frag_4());
+                if(Anim_Flag!=2) {
+                    Anim_Flag = 2;
+                    FG_4.setBackgroundResource(R.drawable.bt_second);
+                    FG_3.setBackgroundResource(R.drawable.bt_first);
+                    replaceFrag(new Frag_4());
+                }
             }
         });
     }
     public void replaceFrag(Fragment fragment){
         FragmentManager fm=getSupportFragmentManager();
         FragmentTransaction ft=fm.beginTransaction();
+        ft.setCustomAnimations(R.anim.l_in,R.anim.l_out,R.anim.r_in,R.anim.r_out);
         ft.replace(R.id.fragment,fragment);
         ft.commit();
     }
